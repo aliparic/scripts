54d4236f9b9cc715b6a89babec53e75736c639dd
Ali Parichereh <aliparic@MonroeSshp.monroe-project.eu>
2018-01-24 10:57:51 +0000

    first commit

--- script_ss_rmbt.sh
+++ script_ss_rmbt.sh
@@ -1,3 +1,3 @@
-docker run -it -d --name nettest --net=host  -v /monroe/config.rmbt:/monroe/config:ro -v /monroe/results:/monroe/results  aliparic/repo1
-docker run -it -d --name ali_ping --net=host  -v /monroe/config.ping:/monroe/config:ro -v /monroe/results:/monroe/results  aliparic/repo2
+docker run -it -d --name nettest --net=host  -v /monroe/scripts/config.rmbt:/monroe/config:ro -v /monroe/results:/monroe/results  aliparic/repo1
+docker run -it -d --name ali_ping --net=host  -v /monroe/scripts/config.ping:/monroe/config:ro -v /monroe/results:/monroe/results  aliparic/repo2
 /monroe/ssTrace.sh
