cabe5d3019a25272443aacea62c8365e02e5bd64
Ali Parichereh <aliparic@MonroeSshp.monroe-project.eu>
2018-01-25 09:14:32 +0000

    first commit

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-for i in {1..5}
+cc="reno cubic bbr"
+for c in $cc
+do
+echo $c > /proc/sys/net/ipv4/tcp_congestion_control
+for i in {1..10}
 do
    sh ./script_ss_rmbt.sh
    docker rm -f nettest
    docker rm -f ali_ping
    rm /monroe/results/*PING*
+done
 done
--- ssTrace.sh
+++ ssTrace.sh
@@ -3,7 +3,7 @@
 #base=1
 today=`date +%Y%m%d-%H%M%S`
 touch  /monroe/ssout/ss$today.txt
-secs=80
+secs=60
 endTime=$(( $(date +%s) + secs ))
 while [ $(date +%s) -lt $endTime ]
 do
